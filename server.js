161f1efa485c0f951e4e0e4c952b792468123d53
melih sancar <melixyz@gmail.com>
2026-03-25 03:25:28 +0300

    mail revize 3

--- server.js
+++ server.js
@@ -169,10 +169,15 @@ app.get("/offers", (req, res) => {
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: "melixyz@gmail.com",
     pass: process.env.MAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
