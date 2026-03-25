6d6226892784c661d9ae535968d09d6c4b876b13
melih sancar <melixyz@gmail.com>
2026-03-25 19:24:20 +0300

    mysql2 revize

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ dotenv.config();
 
 // 🔹 DB CONNECTION (EN ÜSTTE)
 const db = await mysql.createPool({
-  host: process.env.DB_HOST,
+  import mysql from "mysql2/promise";
+
+const db = await mysql.createPool(process.env.DATABASE_URL);
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
